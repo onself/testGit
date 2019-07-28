21dcc35cd3e11351ee00434ad962d9b5b02c0157
hd <hd@DESKTOP-9AIOKE5>
2019-07-28 15:19:17 +0800

    has modify

--- src/main/java/com/example/git/Git.java
+++ src/main/java/com/example/git/Git.java
@@ -3,6 +3,6 @@ package com.example.git;
 public class Git {
 
 	public static void main(String[] args) {
-		System.out.println("hello ,This is first git project");
+		System.out.println("hello ,This is first git project has modify");
 	}
 }
